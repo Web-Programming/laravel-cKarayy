eb14d1e6f62bccbdaa26ac2a4e68c645cbfa0c11
Callista Virginia <146695862+cKarayy@users.noreply.github.com>
2023-10-09 13:13:04 +0700

    Route

---
+++ callistaV2226250118/resources/views/profile.blade.php
@@ -0,0 +1,1 @@
+<h1> Olaa, mi chiamo karay
--- callistaV2226250118/routes/web.php
+++ callistaV2226250118/routes/web.php
@@ -16,3 +16,47 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//buat route ke halaman profile
+Route::get('/profile', function () {
+    return view('Profile');
+});
+
+//pm wajib
+Route::get('/mahasiswa/{nama?}', function ($nama = 'karay') {
+   echo "<h2>Olaa epribadi, mi chiamo $nama</h2>";
+});
+
+//pm ga wajib
+// Route::get('/mahasiswa/{nama?}', function ($nama = 'karay') {
+//    echo "<h2>Olaa epribadi, mi chiamo $nama</h2>";
+// });
+
+//pm >1
+Route::get('/mahasiswa/{nama?}/(pekerjaan?', function ($nama = 'karay', $pekerjaan = "Pilot") {
+   echo "<h2>Olaa epribadi, mi chiamo $nama as a $pekerjaan</h2>";
+});
+
+//redirect
+Route::get('/hubungi', function () {
+   echo "<h1>Contact us</h2>";
+});
+
+Route::redirect('contact', '/hubungi');
+
+Route::get('/halo', function () {
+   echo "<a href='" . route('call') . "'>" . route('call') . "</a";
+});
+
+//route per modul
+Route::prefix('/dosen')->group(function () {
+    Route::get('/jadwal', function () {
+         return "<h1>Jadwal<h1>";
+    });
+
+    Route::get('/materi', function(){
+        return "<h1>Materi Perkuliahan<h2>";
+    });
+});
+
+
